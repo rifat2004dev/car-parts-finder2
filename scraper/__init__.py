8191f0532ca0c8fae7304c24df24f40c59b8ad30
rifat2004dev <rifatbrinkhuis2004@gmail.com>
2026-03-20 11:36:10 +0200

    Refactor scraper imports and registry
    Updated the scraper registry to include new scrapers and removed old ones.

--- scraper/__init__.py
+++ scraper/__init__.py
@@ -1,11 +1,26 @@
-from .gumtree_scraper import search_gumtree
-from .midas_scraper import search_midas
-from .masterparts_scraper import search_masterparts
-from .modern_auto_parts_scraper import search_modern
+from scraper.atlantic_auto_scraper import AtlanticAutoSparesScraper
+from scraper.autozone_scraper import AutoZoneScraper
+from scraper.berlin_car_parts_scraper import BerlinCarPartsScraper
+from scraper.boss_autospares_scraper import BossAutoSparesScraper
+from scraper.facebook_scraper import FacebookMarketplaceScraper
+from scraper.gumtree_scraper import GumtreeScraper
+from scraper.masterparts_scraper import MasterpartsScraper
+from scraper.midas_scraper import MidasScraper
+from scraper.modern_auto_parts_scraper import ModernAutoPartsScraper
+from scraper.onlinecarparts_scraper import OnlineCarPartsScraper
+from scraper.takealot_scraper import TakealotScraper
 
 SCRAPER_REGISTRY = [
-    search_gumtree,
-    search_midas,
-    search_masterparts,
-    search_modern
+    GumtreeScraper,
+    FacebookMarketplaceScraper,
+    ModernAutoPartsScraper,
+    BerlinCarPartsScraper,
+    OnlineCarPartsScraper,
+    TakealotScraper,
+    AutoZoneScraper,
+    MidasScraper,
+    MasterpartsScraper,
+    BossAutoSparesScraper,
+    AtlanticAutoSparesScraper,
 ]
+
